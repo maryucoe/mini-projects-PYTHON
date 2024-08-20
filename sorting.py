5a057a9a6e34d30c1094f3f22e7a58aed2baa6f8
Mary <119369807+maryucoe@users.noreply.github.com>
2024-08-20 07:29:41 +0300

    init

---
+++ sorting.py
@@ -0,0 +1,9 @@
+import pygame
+pygame.init()
+window=pygame.display.set_mode((500,400))
+pygame.display.set_caption("Sorting")
+x=40
+y=40
+width=20
+height = [200, 50, 130, 90, 250, 61, 110, 88, 33, 80, 70, 159, 180, 20]
+run=True
